80e973790b8e070b8fefbf536dc8581c1531602e
Philip Chimento <pchimento@igalia.com>
2023-04-19 15:06:20 -0700

    Remove unapplied patches
    These are commented out on all builds, so no need to carry around the
    patch files. Some of them seem like they don't even apply to current V8.
    
    I'm keeping the v8_shared_library patches for now, since those seem like
    they might be useful for experimentation unless they get too stale.

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -8,41 +8,20 @@ V8_PATCHSET_ANDROID=(
   # V8 shared library support
   # "v8_shared_library.patch"
 
-  # https://github.com/Kudo/react-native-v8/issues/27
-  # "workaround_jsi_object_freeze.patch"
-
-  # Support to specify custom timezone
-  # https://github.com/Kudo/react-native-v8/issues/37
-  # "custom_timezone.patch"
-
   # Fix v8 9.7 build error
   "v8_97_android_build_error.patch"
 
   # Fix v8 9.7 libunwind link error
   # revert https://chromium.googlesource.com/chromium/src/build/+/7bb5f36104
   "v8_97_android_unwind_link_error.patch"
-
-  # Fix for [react-native-bottom-sheet](https://github.com/gorhom/react-native-bottom-sheet) not working
-  # revert https://chromium-review.googlesource.com/c/v8/v8/+/3548458
-  # "fix_for_bottom_sheet.patch"
 )
 
 V8_PATCHSET_IOS=(
   # V8 shared library support
   # "v8_shared_library_ios.patch"
 
-  # https://github.com/Kudo/react-native-v8/issues/27
-  # "workaround_jsi_object_freeze.patch"
-
-  # Workaround latest Xcode12 build break on non Apple Silicon
-  # "v8_ios_host_break.patch"
-
   # Fix use_system_xcode build error
   "system_xcode_build_error.patch"
-
-  # Fix for [react-native-bottom-sheet](https://github.com/gorhom/react-native-bottom-sheet) not working
-  # revert https://chromium-review.googlesource.com/c/v8/v8/+/3548458
-  # "fix_for_bottom_sheet.patch"
 
   "arm64_catalyst.patch"
 
